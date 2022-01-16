3cf8b7ad614fc79502a7144fc0063a7fc290d0ae
Mohamed Aladem <alademm@gmail.com>
2022-01-15 23:25:47 -0500

    Use spaces.

--- MikroPF.h
+++ MikroPF.h
@@ -31,92 +31,92 @@
 class MikroPF
 {
 public:
-	using Real = double; // Change the floating point type used by the filter.
+  using Real = double; // Change the floating point type used by the filter.
 
-	// Your callbacks will be passed references to ParticleState objects.
-	// You can then use them to read/write the state of particles.
-	struct ParticleState
-	{
-	public:
-		// idx must be in range [0, state_dim).
-		inline Real GetAt(const int idx)const;
-		inline ParticleState& SetAt(const int idx, Real val);
+  // Your callbacks will be passed references to ParticleState objects.
+  // You can then use them to read/write the state of particles.
+  struct ParticleState
+  {
+  public:
+    // idx must be in range [0, state_dim).
+    inline Real GetAt(const int idx)const;
+    inline ParticleState& SetAt(const int idx, Real val);
 
-	private:
-		friend class MikroPF;
-		std::vector<Real>* m_state;
-		int m_begin, m_end;
-	};
+  private:
+    friend class MikroPF;
+    std::vector<Real>* m_state;
+    int m_begin, m_end;
+  };
 
-	// The prior function will be used to initialize the state of each particle. Might be called from multiple threads.
-	using PriorFn = void(*)(ParticleState* particle_state);
+  // The prior function will be used to initialize the state of each particle. Might be called from multiple threads.
+  using PriorFn = void(*)(ParticleState* particle_state);
 
-	// Used to update the state. Might be called from multiple threads.
-	using PredictionFn = void(*)(ParticleState* particle_state, void* optional_data);
+  // Used to update the state. Might be called from multiple threads.
+  using PredictionFn = void(*)(ParticleState* particle_state, void* optional_data);
 
-	// Compute likelihood p(z | sample), i.e. p(measurement | particle_state). The result out_likelihood is a single
-	// number between [0,1]. Might be called from multiple threads.
-	using LikelihoodFn = void(*)(const ParticleState* particle_state, const Real* measurement, Real* out_likelihood, void* optional_data);
+  // Compute likelihood p(z | sample), i.e. p(measurement | particle_state). The result out_likelihood is a single
+  // number between [0,1]. Might be called from multiple threads.
+  using LikelihoodFn = void(*)(const ParticleState* particle_state, const Real* measurement, Real* out_likelihood, void* optional_data);
 
-	// Resample and report the result in out_indices. out_indices will be pre-allocated to the same size as the input
-	// weights. Each element of out_indices specifies which particle to use for that position in the resample. For example, 
-	// assuming we have 3 particles and out_indices is {0, 0, 2} this means copy the current particle_0 for the first 2 particles
-	// of the resample, particle_1 will be dropped and current particle_2 will be copied as is.
-	using ResamplingFn = void(*)(const std::vector<Real>& weights, std::vector<int>* out_indices);
+  // Resample and report the result in out_indices. out_indices will be pre-allocated to the same size as the input
+  // weights. Each element of out_indices specifies which particle to use for that position in the resample. For example, 
+  // assuming we have 3 particles and out_indices is {0, 0, 2} this means copy the current particle_0 for the first 2 particles
+  // of the resample, particle_1 will be dropped and current particle_2 will be copied as is.
+  using ResamplingFn = void(*)(const std::vector<Real>& weights, std::vector<int>* out_indices);
 
-	MikroPF();
-	~MikroPF();
+  MikroPF();
+  ~MikroPF();
 
-	// Initializes the filter. Returns true on success.
-	bool init(
-		int state_dim,
-		int num_particles,
-		PriorFn prior_fn,
-		PredictionFn prediction_fn,
-		LikelihoodFn likelihood_fn,
-		ResamplingFn resampling_fn,
-		Real effective_sample_size_th, // Perform resampling if effective sample size drops below this threshold. Range [0, 1].
-		int num_threads = -1 // A negative number indicates auto-detect the number of threads. 
-	);
+  // Initializes the filter. Returns true on success.
+  bool init(
+    int state_dim,
+    int num_particles,
+    PriorFn prior_fn,
+    PredictionFn prediction_fn,
+    LikelihoodFn likelihood_fn,
+    ResamplingFn resampling_fn,
+    Real effective_sample_size_th, // Perform resampling if effective sample size drops below this threshold. Range [0, 1].
+    int num_threads = -1 // A negative number indicates auto-detect the number of threads. 
+  );
 
-	// Update the filter. The new measurement will be incorporated if available. The measurement here will be
-	// passed as-is to the LikelihoodFn, the filter itself imposes no requirements on its dimensions.
-	void update(const Real* measurement = nullptr, void* optional_data = nullptr);
+  // Update the filter. The new measurement will be incorporated if available. The measurement here will be
+  // passed as-is to the LikelihoodFn, the filter itself imposes no requirements on its dimensions.
+  void update(const Real* measurement = nullptr, void* optional_data = nullptr);
 
-	// Apply a function on each particle and its weight. This will be called from the main thread.
-	void apply(std::function<void(ParticleState* particle_state, Real* particle_weight)> callback);
+  // Apply a function on each particle and its weight. This will be called from the main thread.
+  void apply(std::function<void(ParticleState* particle_state, Real* particle_weight)> callback);
 
-	// Reinitializes the particles using the prior_fn and resets their weights.
-	void reset();
+  // Reinitializes the particles using the prior_fn and resets their weights.
+  void reset();
 
-	// ------------------
-	// Resampling methods
-	// ------------------
-	static void systematic_resampling(const std::vector<Real>& weights, std::vector<int>* out_indices);
+  // ------------------
+  // Resampling methods
+  // ------------------
+  static void systematic_resampling(const std::vector<Real>& weights, std::vector<int>* out_indices);
 
-	class ExecutionEngine; // internal - don't worry about it.
+  class ExecutionEngine; // internal - don't worry about it.
 
 private:
-	std::unique_ptr<ExecutionEngine> m_engine;
-	void(*m_update_cb)(ExecutionEngine* e, const Real* measurement, void* optional_data) = nullptr;
+  std::unique_ptr<ExecutionEngine> m_engine;
+  void(*m_update_cb)(ExecutionEngine* e, const Real* measurement, void* optional_data) = nullptr;
 };
 
 // ==================================================================================
 inline MikroPF::ParticleState& MikroPF::ParticleState::SetAt(const int idx, Real val)
 {
-	const int i = m_begin + idx;
-	assert(i >= m_begin && i < m_end);
-	if (i >= m_begin && i < m_end)
-	{
-		(*m_state)[i] = val;
-	}
-	return *this;
+  const int i = m_begin + idx;
+  assert(i >= m_begin && i < m_end);
+  if (i >= m_begin && i < m_end)
+  {
+    (*m_state)[i] = val;
+  }
+  return *this;
 }
 
 inline MikroPF::Real MikroPF::ParticleState::GetAt(const int idx)const
 {
-	const int i = m_begin + idx;
-	assert(i >= m_begin && i < m_end);
-	return ((i >= m_begin && i < m_end) ? (*m_state)[i] : std::numeric_limits<Real>::infinity());
+  const int i = m_begin + idx;
+  assert(i >= m_begin && i < m_end);
+  return ((i >= m_begin && i < m_end) ? (*m_state)[i] : std::numeric_limits<Real>::infinity());
 }
 
--- example.cpp
+++ example.cpp
@@ -11,97 +11,97 @@ using Real = MikroPF::Real;
 
 void prior_fn(MikroPF::ParticleState* particle_state)
 {
-	// Initialize from a gaussian.
-	thread_local std::default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());
-	std::normal_distribution<Real> norm(0.0, 1.0);
-	particle_state->SetAt(0, norm(generator)).SetAt(1, norm(generator)).SetAt(2, norm(generator));
+  // Initialize from a gaussian.
+  thread_local std::default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());
+  std::normal_distribution<Real> norm(0.0, 1.0);
+  particle_state->SetAt(0, norm(generator)).SetAt(1, norm(generator)).SetAt(2, norm(generator));
 }
 
 void prediction_fn(MikroPF::ParticleState* particle_state, void* /*optional_data*/)
 {
-	// Simple linear dynamics model with added gaussian noise.
-	constexpr Real dt = 0.25;
-	constexpr Real noise_sigma = 0.125;
-	thread_local std::default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());
-	std::normal_distribution<Real> norm(0.0, noise_sigma);
-	const Real x = particle_state->GetAt(0);
-	const Real v = particle_state->GetAt(1);
-	const Real a = particle_state->GetAt(2);
-	Real new_a = a + norm(generator);
-	Real new_v = v + a * dt + norm(generator);
-	Real new_x = x + v * dt + 0.5 * a * dt * dt + norm(generator);
-	particle_state->SetAt(0, new_x).SetAt(1, new_v).SetAt(2, new_a);
+  // Simple linear dynamics model with added gaussian noise.
+  constexpr Real dt = 0.25;
+  constexpr Real noise_sigma = 0.125;
+  thread_local std::default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());
+  std::normal_distribution<Real> norm(0.0, noise_sigma);
+  const Real x = particle_state->GetAt(0);
+  const Real v = particle_state->GetAt(1);
+  const Real a = particle_state->GetAt(2);
+  Real new_a = a + norm(generator);
+  Real new_v = v + a * dt + norm(generator);
+  Real new_x = x + v * dt + 0.5 * a * dt * dt + norm(generator);
+  particle_state->SetAt(0, new_x).SetAt(1, new_v).SetAt(2, new_a);
 }
 
 void likelihood_fn(const MikroPF::ParticleState* particle_state, const Real* measurement,
-	Real* out_likelihood, void* /*optional_data*/)
+  Real* out_likelihood, void* /*optional_data*/)
 {
-	constexpr Real noise_sigma = 1.0;
-	const Real error = particle_state->GetAt(0) - *measurement;
-	// Map the error to a probability using an RBF kernel.
-	*out_likelihood = std::exp(-(error * error) / (2.0f * noise_sigma * noise_sigma));
+  constexpr Real noise_sigma = 1.0;
+  const Real error = particle_state->GetAt(0) - *measurement;
+  // Map the error to a probability using an RBF kernel.
+  *out_likelihood = std::exp(-(error * error) / (2.0f * noise_sigma * noise_sigma));
 }
 
 int main()
 {
-	// Tracking a sinusoidal. This example is adapted from: https://github.com/johnhw/pfilter/blob/master/examples/timeseries.ipynb
-	constexpr int N = 100;
-	std::vector<int> x(N);
-	std::vector<Real> y_true(N);
-	std::vector<Real> y_noisy(N);
-	std::default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());
-	// Fill the data.
-	{
-		std::normal_distribution<Real> norm(0.0, 0.5);
-		for (int i = 0; i < N; ++i)
-		{
-			x[i] = i;
-			y_true[i] = std::cos(0.25 * x[i]) + x[i] * 0.05;
-			y_noisy[i] = y_true[i] + norm(generator);
-		}
-	}
+  // Tracking a sinusoidal. This example is adapted from: https://github.com/johnhw/pfilter/blob/master/examples/timeseries.ipynb
+  constexpr int N = 100;
+  std::vector<int> x(N);
+  std::vector<Real> y_true(N);
+  std::vector<Real> y_noisy(N);
+  std::default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());
+  // Fill the data.
+  {
+    std::normal_distribution<Real> norm(0.0, 0.5);
+    for (int i = 0; i < N; ++i)
+    {
+      x[i] = i;
+      y_true[i] = std::cos(0.25 * x[i]) + x[i] * 0.05;
+      y_noisy[i] = y_true[i] + norm(generator);
+    }
+  }
 
-	// Create and initialize our filter.
-	MikroPF pf;
-	bool result = pf.init(
-		/*state_dim*/ 3, // state is [x, dx, ddx]
-		/*num_particles*/ 2000,
-		prior_fn,
-		prediction_fn,
-		likelihood_fn,
-		MikroPF::systematic_resampling,
-		/*effective_sample_size_th*/ 0.25,
-		/*num_threads*/ -1);
-	if (!result)
-	{
-		std::cout << "Particle filter initialization failed." << std::endl;
-		return -1;
-	}
+  // Create and initialize our filter.
+  MikroPF pf;
+  bool result = pf.init(
+    /*state_dim*/ 3, // state is [x, dx, ddx]
+    /*num_particles*/ 2000,
+    prior_fn,
+    prediction_fn,
+    likelihood_fn,
+    MikroPF::systematic_resampling,
+    /*effective_sample_size_th*/ 0.25,
+    /*num_threads*/ -1);
+  if (!result)
+  {
+    std::cout << "Particle filter initialization failed." << std::endl;
+    return -1;
+  }
 
-	std::uniform_real_distribution<Real> dist(0, 1);
-	std::vector<Real> estimated_positions(N);
-	for (int i = 0; i < N; ++i)
-	{
-		// Note that our measurement here is a single floating point value but that need not be the case. The measurement could be an array of values for example.
-		Real* measurement = dist(generator) > 0.25 ? &y_noisy[i] : nullptr; // Randomly drop some measurements to test the filter result with missing values.
-		pf.update(measurement);
-		// Now we apply a function to compute our mean estimated position value.
-		std::vector<MikroPF::Real> mean_state(3, 0.0);
-		pf.apply([&mean_state](MikroPF::ParticleState* particle_state, Real* particle_weight) {
-			mean_state[0] += (*particle_weight * particle_state->GetAt(0));
-			mean_state[1] += (*particle_weight * particle_state->GetAt(1));
-			mean_state[2] += (*particle_weight * particle_state->GetAt(2));
-			});
-		estimated_positions[i] = mean_state[0];
-	}
+  std::uniform_real_distribution<Real> dist(0, 1);
+  std::vector<Real> estimated_positions(N);
+  for (int i = 0; i < N; ++i)
+  {
+    // Note that our measurement here is a single floating point value but that need not be the case. The measurement could be an array of values for example.
+    Real* measurement = dist(generator) > 0.25 ? &y_noisy[i] : nullptr; // Randomly drop some measurements to test the filter result with missing values.
+    pf.update(measurement);
+    // Now we apply a function to compute our mean estimated position value.
+    std::vector<MikroPF::Real> mean_state(3, 0.0);
+    pf.apply([&mean_state](MikroPF::ParticleState* particle_state, Real* particle_weight) {
+      mean_state[0] += (*particle_weight * particle_state->GetAt(0));
+      mean_state[1] += (*particle_weight * particle_state->GetAt(1));
+      mean_state[2] += (*particle_weight * particle_state->GetAt(2));
+      });
+    estimated_positions[i] = mean_state[0];
+  }
 
-	// Write into a csv file for plotting.
-	{
-		std::ofstream file("result.csv");
-		file << "x,y_true,y_noisy,pf" << std::endl;
-		for (int i = 0; i < N; ++i)
-		{
-			file << x[i] << "," << y_true[i] << "," << y_noisy[i] << "," << estimated_positions[i] << std::endl;
-		}
-	}
+  // Write into a csv file for plotting.
+  {
+    std::ofstream file("result.csv");
+    file << "x,y_true,y_noisy,pf" << std::endl;
+    for (int i = 0; i < N; ++i)
+    {
+      file << x[i] << "," << y_true[i] << "," << y_noisy[i] << "," << estimated_positions[i] << std::endl;
+    }
+  }
 }
